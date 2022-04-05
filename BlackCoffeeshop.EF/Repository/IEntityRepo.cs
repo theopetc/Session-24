593a543f593dc75b13a726b1841ea8caa8d53420
Theodoros Petsagkas <tpetsagkas@gmail.com>
2022-04-05 13:42:05 +0300

    Deleted mock repos

--- BlackCoffeeshop.EF/Repository/IEntityRepo.cs
+++ BlackCoffeeshop.EF/Repository/IEntityRepo.cs
@@ -9,14 +9,7 @@ namespace BlackCoffeeshop.EF.Repository
 {
     public interface IEntityRepo<TEntity>
            where TEntity : BaseEntity {
-        List<TEntity> GetAll();
-
-        TEntity? GetById(int id);
-        Task Create(TEntity entity);
-        Task Update(int id, TEntity entity);
-        Task Delete(int id);
-
-
+       
         Task<IEnumerable<TEntity>> GetAllAsync();
         Task<TEntity?> GetByIdAsync(int id);
         Task CreateAsync(TEntity entity);
