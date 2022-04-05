7f63b69ebf55eae8c09dc1402d5b4725e0a9146d
Theodoros Petsagkas <tpetsagkas@gmail.com>
2022-04-05 13:51:32 +0300

    .

--- Session-24.Blazor/BlackCoffeeshop.Model/Customer.cs
+++ Session-24.Blazor/BlackCoffeeshop.Model/Customer.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 namespace BlackCoffeeshop.Model;
 
 public class Customer : BaseEntity {
+
     public string Code { get; set; }
     public string Description { get; set; } = string.Empty;
-    public Transaction Transaction { get; set; }
+    public List<Transaction> Transactions { get; set; }
 
 }
